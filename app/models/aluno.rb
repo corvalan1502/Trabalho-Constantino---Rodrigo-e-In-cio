1df0b10575b59f8a7f6a2bb99af88368d3a5b06f
corvalan1502 <guigocorvalan@hotmail.com>
2026-06-05 15:32:52 -0300

    Validacoes adicionadas nos modelos

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -1,3 +1,6 @@
 class Aluno < ApplicationRecord
-    has_many :matriculas
+  has_many :matriculas
+  validates :nome, presence: true
+  validates :cpf, presence: true, uniqueness: true
+  validates :data_nascimento, presence: true
 end
--- app/models/matricula.rb
+++ app/models/matricula.rb
@@ -1,4 +1,5 @@
 class Matricula < ApplicationRecord
   belongs_to :aluno
   belongs_to :disciplina
+  validates :aluno_id, uniqueness: { scope: :disciplina_id }
 end
